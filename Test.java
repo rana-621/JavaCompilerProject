8834ec4de59a1103d37aa62863246a58e92921fb
rana-621 <ranaebrahim621@gmail.com>
2024-12-26 20:31:45 +0200

    first commit for compiler using java

---
+++ Test.java
@@ -0,0 +1,11 @@
+public class HelloWorld {
+    public static void main(string[] args) {
+        string message = "Hello, World!";
+        int number = 10;
+        if (number > 5) {
+            System.out.println(message + " Number is greater than 5.");
+        } else {
+            System.out.println(message + " Number is less than or equal to 5.");
+        }
+    }
+}
